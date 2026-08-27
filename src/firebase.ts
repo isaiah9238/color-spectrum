6f295079274bd3308f9830cc9064fdaeb337a309
Isaiah Smith <isaiah9238@gmail.com>
2026-08-27 06:02:35 -0400

    firebase.ts and a css file

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
 import { 
   getAuth, 
   GoogleAuthProvider, 
